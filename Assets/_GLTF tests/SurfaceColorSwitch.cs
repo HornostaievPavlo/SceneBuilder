2cd88fbdb9ddba855f97a7b6248ed0b00c98a129
HornostaievPavlo <goronostaev@gmail.com>
2023-06-07 16:41:42 +0300

    Added texture changing through script

--- Assets/_GLTF tests/SurfaceColorSwitch.cs
+++ Assets/_GLTF tests/SurfaceColorSwitch.cs
@@ -12,6 +12,11 @@ public class SurfaceColorSwitch : MonoBehaviour
 
     [SerializeField] private Color targetColor;
 
+    [SerializeField] private Texture defaultTexture;
+
+    private const string BASE_COLOR = "baseColorFactor";
+    private const string TEXTURE = "baseColorTexture";
+
     private void Awake()
     {
         targetRenderer = target.GetComponent<MeshRenderer>();
@@ -19,15 +24,54 @@ public class SurfaceColorSwitch : MonoBehaviour
         targetMaterial = targetRenderer.material;
 
         defaultColor = targetMaterial.color;
+
+        defaultTexture = targetMaterial.GetTexture(TEXTURE);
+
+
     }
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            targetMaterial.SetColor("baseColorFactor", targetColor); //glTF
+            targetMaterial.SetColor(BASE_COLOR, targetColor); //glTF
 
             //targetMaterial.SetColor("_Color", targetColor); //standard
         }
+        else if (Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.Z))
+        {
+            targetMaterial.SetColor(BASE_COLOR, defaultColor);
+        }
+        else if (Input.GetKeyDown(KeyCode.A)) // adding tint to texture
+        {
+            Color multiplier = new Color(defaultColor.r * 0.5f,
+                                         defaultColor.g * 0.5f,
+                                         defaultColor.b * 0.5f);
+
+            targetMaterial.SetColor(BASE_COLOR, multiplier);
+        }
+        else if (Input.GetKeyDown(KeyCode.F))
+        {
+            targetMaterial.SetTexture(TEXTURE, null);
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            targetMaterial.SetTexture(TEXTURE, defaultTexture);
+        }
     }
 }
+
+
+
+
+
+
+
+
+/*
+1) color picker на все цвета 
+поставить палитру 
+и смотреть по ray cast в UV coordinate, потом в цвет пикселя
+
+2) оттенки множители в Color - 48
+ */
