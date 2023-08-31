6ec14e3ed03ab9286aad7011c02fbe83779372cf
Pavlo Hornostaiev <goronostaev@gmail.com>
2023-08-31 11:01:22 +0300

    Added new Assembly Definition and set up references. Added initial code for saving system.

---
+++ Assets/Scripts/Systems/Saving System/SaveLoadUtility.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using UnityEngine;
+public class SaveLoadUtility : MonoBehaviour
+{
+    public static readonly string modelPath = @"D:\_GLTF\Saves\Asset\Model.gltf";
+    public static readonly string texturePath = @"D:\_GLTF\Saves\Asset\Texture.png";
+    public static readonly string duckModelPath =
+        "https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/Duck/glTF/Duck.gltf";
+
+    public static Transform placeholder;
+
+    public static List<GameObject> savingTargets = new List<GameObject>();
+
+    private void Awake()
+    {
+        placeholder = GameObject.Find("Placeholder").transform;
+    }
+
+    public static void ClearTargetsList()
+    {
+        savingTargets.Clear();
+    }
+}
--- Assets/Scripts/Systems/Saving System/SavingSystem.cs
+++ Assets/Scripts/Systems/Saving System/SavingSystem.cs
@@ -1,18 +1,88 @@
-using System.Collections;
-using System.Collections.Generic;
+using GLTFast.Export;
+using System.IO;
 using UnityEngine;
 
-public class SavingSystem : MonoBehaviour
+public class SavingSystem : MonoBehaviour//, ISavable
 {
-    // Start is called before the first frame update
-    void Start()
+    private void CollectSelectableObjects()
     {
-        
+        SelectableObject[] selectableObjects =
+            SaveLoadUtility.placeholder.GetComponentsInChildren<SelectableObject>();
+
+        foreach (var item in selectableObjects)
+        {
+            SaveLoadUtility.savingTargets.Add(item.gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SaveAsset()
     {
-        
+        CollectSelectableObjects();
+
+        SaveTexture();
+
+        SaveModel();
+    }
+
+    private async void SaveModel()
+    {
+        var export = new GameObjectExport();
+        export.AddScene(SaveLoadUtility.savingTargets.ToArray());
+
+        bool success = await export.SaveToFileAndDispose(SaveLoadUtility.modelPath);
+
+        if (success) Debug.Log("Model saved successfully");
+    }
+
+    private void SaveTexture()
+    {
+        Renderer renderer = SaveLoadUtility.savingTargets[0].GetComponentInChildren<Renderer>();
+
+        if (renderer != null)
+        {
+            Material material = renderer.sharedMaterial;
+
+            if (material != null && material.mainTexture != null)
+            {
+                Texture2D texture = (Texture2D)material.mainTexture;
+
+                Texture2D readableCopy = DuplicateTexture(texture);
+
+                SaveTextureToFile(readableCopy);
+
+                Debug.Log("Texture saved successfully");
+            }
+        }
+
+        //foreach (var target in savingTargets)
+        //{
+        //    Destroy(target);
+        //}
+    }
+
+    private void SaveTextureToFile(Texture2D texture)
+    {
+        byte[] textureBytes = texture.EncodeToPNG();
+        File.WriteAllBytes(SaveLoadUtility.texturePath, textureBytes);
+    }
+
+    private Texture2D DuplicateTexture(Texture2D source)
+    {
+        RenderTexture renderTex = RenderTexture.GetTemporary(
+                    source.width,
+                    source.height,
+                    0,
+                    RenderTextureFormat.Default,
+                    RenderTextureReadWrite.Linear);
+
+        Graphics.Blit(source, renderTex);
+        RenderTexture previous = RenderTexture.active;
+        RenderTexture.active = renderTex;
+        Texture2D readableText = new Texture2D(source.width, source.height);
+        readableText.ReadPixels(new Rect(0, 0, renderTex.width, renderTex.height), 0, 0);
+        readableText.Apply();
+        RenderTexture.active = previous;
+        RenderTexture.ReleaseTemporary(renderTex);
+        return readableText;
     }
 }
