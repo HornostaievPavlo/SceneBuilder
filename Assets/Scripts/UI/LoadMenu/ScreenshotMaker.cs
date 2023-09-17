6775aaf8bcb07751f77694c8fefbb0269f7ffeaa
Pavlo Hornostaiev <goronostaev@gmail.com>
2023-09-17 13:19:24 +0300

    Screenshot Maker base implementation.

--- Assets/Scripts/UI/LoadMenu/ScreenshotMaker.cs
+++ Assets/Scripts/UI/LoadMenu/ScreenshotMaker.cs
@@ -1,11 +1,51 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScreenshotMaker : MonoBehaviour
 {
-    private Texture CaptureCameraView()
-    {
-        Texture texture = null;
+    private Camera mainCamera;
 
-        return texture;
+    public RawImage icon;
+
+    private void Awake()
+    {
+        mainCamera = GetComponent<Camera>();
+    }
+
+    public byte[] CaptureCameraView()
+    {
+        var width = Screen.width;
+        var height = Screen.height;
+        var photo = new Texture2D(width, height, TextureFormat.RGBA32, false);
+
+        RenderTexture renderTexture = new RenderTexture(width, height, 24, RenderTextureFormat.ARGB32);
+
+        mainCamera.targetTexture = renderTexture;
+        mainCamera.Render();
+
+        RenderTexture.active = mainCamera.targetTexture;
+        photo.ReadPixels(new Rect(0, 0, width, height), 0, 0);
+
+        mainCamera.targetTexture = null;
+        RenderTexture.active = null;
+        DestroyImmediate(renderTexture);
+
+        photo.Apply();
+
+        var bytes = photo.EncodeToJPG(100);
+        Destroy(photo);
+
+        return bytes;
+    }
+
+    public void TestScreenshot()
+    {        
+        byte[] bytes = CaptureCameraView();
+
+        Texture2D tex = new Texture2D(Screen.width, Screen.height);
+        tex.LoadImage(bytes);
+        tex.Apply();
+
+        icon.texture = tex;
     }
 }
