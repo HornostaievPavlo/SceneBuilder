2b37d1f845ce8ebe3106b49b694d380e15372f94
Pavlo Hornostaiev <goronostaev@gmail.com>
2023-09-17 12:27:44 +0300

    Added class for screenshots.

---
+++ Assets/Scripts/UI/LoadMenu/ScreenshotMaker.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class ScreenshotMaker : MonoBehaviour
+{
+    private Texture CaptureCameraView()
+    {
+        Texture texture = null;
+
+        return texture;
+    }
+}
