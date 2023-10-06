fd542e7ce337d62936471ce9e7676582a5b8bfd4
Pavlo Hornostaiev <goronostaev@gmail.com>
2023-10-06 12:14:08 +0300

    Code cleaning for InputSystem

--- Assets/Scripts/Systems/InputSystem/InputSystem.cs
+++ Assets/Scripts/Systems/InputSystem/InputSystem.cs
@@ -12,10 +12,11 @@ public class InputSystem : MonoBehaviour
 
     public event Action TouchBeginAction = delegate { };
     public event Action TouchReleaseAction = delegate { };
+
     public event Action<Vector2> SecondaryDragAction = delegate { };
     public event Action<Vector2> PrimaryDragAction = delegate { };
     public event Action<float> ZoomAction = delegate { };
-    public event Action<Vector2> TapAction = delegate { };
+
     public event Action<RaycastHit> RayHit;
     public event Action RayMiss;
 
@@ -43,37 +44,28 @@ public class InputSystem : MonoBehaviour
     {
         InputScreenSpacePosition = Input.mousePosition;
         Vector2 delta = Vector2.zero;
+
         // If window was just refocused, previous mouse position will be invalid
         // so keep delta 0 unless we had focus for at least 1 frame
         if (hadFocus)
-        {
             delta = NormalizeScreenVector(Input.mousePosition - previousMousePosition);
-        }
-        // On left mouse button single click - raycast
+
         if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
-        {
             Raycast(Input.mousePosition);
-        }
-        // If the left mouse button is not held - end touch
+
         if (!Input.GetMouseButton(0))
-        {
             EndTouch();
-        }
-        // If the right mouse button is held - rotate
+
         if (Input.GetMouseButton(1))
         {
             BeginTouch();
             PrimaryDragAction(delta * mouseRotRate);
         }
-        // If middle mouse held - pan
+
         else if (Input.GetMouseButton(2))
-        {
             SecondaryDragAction(delta * mousePanRate);
-        }
         else if (Input.mouseScrollDelta.y != 0f)
-        {
             ZoomAction(Input.mouseScrollDelta.y * mouseZoomRate);
-        }
     }
 
     private void BeginTouch()
@@ -104,20 +96,22 @@ public class InputSystem : MonoBehaviour
     /// <summary>
     /// Applies panning from axis values.
     /// </summary>
-    public void AxisPan(float horizontal, float vertical)
+    private void AxisPan(float horizontal, float vertical)
     {
         Vector2 delta = new Vector2(horizontal * Time.deltaTime, vertical * Time.deltaTime);
         delta *= axisPanRate;
+
         SecondaryDragAction(delta);
     }
 
     /// <summary>
     /// Applies rotation from axis values.
     /// </summary>
-    public void AxisRotate(float horizontal, float vertical)
+    private void AxisRotate(float horizontal, float vertical)
     {
         Vector2 delta = new Vector2(horizontal * Time.deltaTime, vertical * Time.deltaTime);
         delta *= axisRotRate;
+
         PrimaryDragAction(delta);
     }
 
@@ -129,9 +123,8 @@ public class InputSystem : MonoBehaviour
         if (RayHit == null) return;
 
         Ray ray = mainCamera.ScreenPointToRay(screenPosition);
-        RaycastHit hit;
 
-        if (Physics.Raycast(ray, out hit, mainCamera.farClipPlane))
+        if (Physics.Raycast(ray, out RaycastHit hit, mainCamera.farClipPlane))
             RayHit(hit);
         else
             RayMiss();
