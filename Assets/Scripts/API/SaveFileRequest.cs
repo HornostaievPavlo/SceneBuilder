7f53967c18ed28b3c0b203702bde2783353dc049
Pavlo Hornostaiev <goronostaev@gmail.com>
2023-11-15 19:30:44 +0200

    Removed API integration

--- Assets/Scripts/API/SaveFileRequest.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-[Serializable]
-public class SaveFileRequest
-{
-    public int Id;
-
-    public byte[] PreviewData;
-}
--- Assets/Scripts/API/SaveFileResponse.cs
+++
@@ -1,4 +0,0 @@
-public class SaveFileResponse
-{
-    public bool IsSaved;
-}
