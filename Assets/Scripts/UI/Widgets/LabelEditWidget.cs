98b8d08d963fcfdd41867817455d86e4fe488e17
Pavlo Hornostaiev <goronostaev@gmail.com>
2025-09-28 16:50:44 +0300

    Added label edit widget

---
+++ Assets/Scripts/UI/Widgets/LabelEditWidget.cs
@@ -0,0 +1,13 @@
+﻿using Gameplay;
+using UnityEngine;
+
+namespace UI.Widgets
+{
+	public class LabelEditWidget : MonoBehaviour
+	{
+		public void Setup(Label label)
+		{
+			gameObject.SetActive(true);
+		}
+	}
+}
