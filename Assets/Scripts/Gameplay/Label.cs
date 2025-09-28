9d2007c0c013f003c0b05a407690169912435557
Pavlo Hornostaiev <goronostaev@gmail.com>
2025-09-28 16:48:51 +0300

    Added label class

---
+++ Assets/Scripts/Gameplay/Label.cs
@@ -0,0 +1,21 @@
+﻿namespace Gameplay
+{
+	public class Label : SceneObject
+	{
+		private string _title;
+		private string _description;
+		
+		public string Title => _title;
+		public string Description => _description;
+		
+		public void SetTitle(string title)
+		{
+			_title = title;
+		}
+		
+		public void SetDescription(string description)
+		{
+			_description = description;
+		}
+	}
+}
