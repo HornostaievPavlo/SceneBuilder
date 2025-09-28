9473d84209ba7381fc91a8daf76ac8c07f94b689
Pavlo Hornostaiev <goronostaev@gmail.com>
2025-09-28 17:14:55 +0300

    Added OnClosed event to update info widget

--- Assets/Scripts/UI/Widgets/LabelEditWidget.cs
+++ Assets/Scripts/UI/Widgets/LabelEditWidget.cs
@@ -1,13 +1,47 @@
-﻿using Gameplay;
+﻿using System;
+using Gameplay;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace UI.Widgets
 {
 	public class LabelEditWidget : MonoBehaviour
 	{
+		[SerializeField] private Button closeButton;
+		[SerializeField] private Button applyButton;
+		
+		private Label _label;
+		
+		public event Action OnClosed;
+		
+		private void OnEnable()
+		{
+			closeButton.onClick.AddListener(HandleCloseButtonClicked);
+			applyButton.onClick.AddListener(HandleApplyButtonClicked);
+		}
+		
+		private void OnDisable()
+		{
+			closeButton.onClick.RemoveListener(HandleCloseButtonClicked);
+			applyButton.onClick.RemoveListener(HandleApplyButtonClicked);
+		}
+
 		public void Setup(Label label)
 		{
 			gameObject.SetActive(true);
+			_label = label;
+		}
+
+		private void HandleCloseButtonClicked()
+		{
+			OnClosed?.Invoke();
+			gameObject.SetActive(false);
+		}
+
+		private void HandleApplyButtonClicked()
+		{
+			OnClosed?.Invoke();
+			gameObject.SetActive(false);
 		}
 	}
 }
