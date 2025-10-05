5e04475a2fd7011e05a2440c61e026bd9e1464c9
Pavlo Hornostaiev <goronostaev@gmail.com>
2025-10-05 15:35:47 +0300

    Added component to create model copy

---
+++ Assets/Scripts/Gameplay/SceneObjectCopyInstantiator.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Gameplay
+{
+	public class SceneObjectCopyInstantiator : MonoBehaviour
+	{
+		public SceneObject CreateCopy(SceneObject originalObject)
+		{
+			Transform selectableTr = originalObject.transform;
+
+			var meshCopy = new Mesh();
+			meshCopy.Clear();
+			meshCopy = selectableTr.GetComponentInChildren<MeshFilter>().mesh;
+			meshCopy.name = "test";
+
+			Material materialCopy = selectableTr.GetComponentInChildren<MeshRenderer>().material;
+			Texture2D textureCopy = IOUtility.DuplicateTexture((Texture2D)materialCopy.mainTexture);
+
+			SceneObject copyObject = Instantiate(originalObject, Vector3.zero, Quaternion.identity, selectableTr.parent);
+
+			copyObject.gameObject.name = originalObject.gameObject.name;
+			copyObject.gameObject.GetComponentInChildren<MeshFilter>().mesh = meshCopy;
+			copyObject.gameObject.GetComponentInChildren<MeshRenderer>().material.mainTexture = textureCopy;
+			copyObject.gameObject.GetComponentInChildren<MeshCollider>().sharedMesh = meshCopy;
+			
+			return copyObject;
+		}
+	}
+}
