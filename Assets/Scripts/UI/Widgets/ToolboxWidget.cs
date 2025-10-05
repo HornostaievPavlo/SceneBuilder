a0ec0ca1608f564937ddc271caeb2fa2985f2719
Pavlo Hornostaiev <goronostaev@gmail.com>
2025-10-05 13:33:16 +0300

    Added toolbox enabling on object selected

--- Assets/Scripts/UI/Widgets/ToolboxWidget.cs
+++ Assets/Scripts/UI/Widgets/ToolboxWidget.cs
@@ -1,11 +1,53 @@
 ﻿using Gameplay;
+using RuntimeHandle;
+using Services.SceneObjectSelection;
 using UnityEngine;
 using UnityEngine.UI;
+using Zenject;
 
 namespace UI.Widgets
 {
 	public class ToolboxWidget : MonoBehaviour
 	{
+		[Header("Holder")]
+		[SerializeField] private GameObject buttonsHolder;
+		
+		
+		
+		private ISceneObjectSelectionService _sceneObjectSelectionService;
+
+		[Inject]
+		private void Construct(ISceneObjectSelectionService sceneObjectSelectionService)
+		{
+			_sceneObjectSelectionService = sceneObjectSelectionService;
+		}
+		
+		private void OnEnable()
+		{
+			_sceneObjectSelectionService.OnObjectSelected += HandleObjectSelected;
+			_sceneObjectSelectionService.OnObjectDeselected += HandleObjectDeselected;
+			
+			
+		}
+		
+		private void OnDisable()
+		{
+			_sceneObjectSelectionService.OnObjectSelected -= HandleObjectSelected;
+			_sceneObjectSelectionService.OnObjectDeselected -= HandleObjectDeselected;
+			
+			
+		}
+
+		private void HandleObjectSelected(SceneObject sceneObject)
+		{
+			buttonsHolder.SetActive(true);
+		}
+
+		private void HandleObjectDeselected()
+		{
+			buttonsHolder.SetActive(false);
+		}
+
 		
 	}
 }
