9e1f0f3e75a8ef15497c83bd922224051e10f2d4
Pavlo Hornostaiev <goronostaev@gmail.com>
2025-10-11 17:07:35 +0300

    Added subscriptions for load and delete buttons

--- Assets/Scripts/UI/Widgets/LocalSaveWidget.cs
+++ Assets/Scripts/UI/Widgets/LocalSaveWidget.cs
@@ -1,5 +1,5 @@
 using LocalSaves;
-using Services.Saving;
+using Services.Loading;
 using UnityEngine;
 using UnityEngine.UI;
 using Zenject;
@@ -9,23 +9,45 @@ namespace UI.Widgets
     public class LocalSaveWidget : MonoBehaviour
     {
         [SerializeField] private RawImage preview;
+        [SerializeField] private Button loadButton;
+        [SerializeField] private Button deleteButton;
+
+        private LocalSave _localSave;
         
-        private ISaveService _saveService;
-
-        // public RawImage preview => GetComponentInChildren<RawImage>();
-
-        public Button loadButton => GetComponentsInChildren<Button>()[0];
-        public Button deleteButton => GetComponentsInChildren<Button>()[1];
+        private ILoadService _loadService;
 
         [Inject]
-        private void Construct(ISaveService saveService)
+        private void Construct(ILoadService loadService)
         {
-            _saveService = saveService;
+            _loadService = loadService;
+        }
+
+        private void OnEnable()
+        {
+            loadButton.onClick.AddListener(HandleLoadButtonClicked);
+            deleteButton.onClick.AddListener(HandleDeleteButtonClicked);
+        }
+        
+        private void OnDisable()
+        {
+            loadButton.onClick.RemoveListener(HandleLoadButtonClicked);
+            deleteButton.onClick.RemoveListener(HandleDeleteButtonClicked);
         }
 
         public void Setup(LocalSave localSave)
         {
+            _localSave = localSave;
             preview.texture = localSave.Preview;
+        }
+
+        private void HandleLoadButtonClicked()
+        {
+            _loadService.LoadLocalSave(_localSave);
+        }
+
+        private void HandleDeleteButtonClicked()
+        {
+            
         }
     }
 }
