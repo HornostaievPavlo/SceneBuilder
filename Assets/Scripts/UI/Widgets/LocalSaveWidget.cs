446a9b513c5ed6eb18cdb90a1b0e8bfc5df13e9b
Pavlo Hornostaiev <goronostaev@gmail.com>
2025-10-11 16:41:04 +0300

    Added local save widget setup

--- Assets/Scripts/UI/Widgets/LocalSaveWidget.cs
+++ Assets/Scripts/UI/Widgets/LocalSaveWidget.cs
@@ -1,15 +1,31 @@
+using LocalSaves;
+using Services.Saving;
 using UnityEngine;
 using UnityEngine.UI;
+using Zenject;
 
 namespace UI.Widgets
 {
     public class LocalSaveWidget : MonoBehaviour
     {
-        public int currentNumber;
+        [SerializeField] private RawImage preview;
+        
+        private ISaveService _saveService;
 
-        public RawImage preview => GetComponentInChildren<RawImage>();
+        // public RawImage preview => GetComponentInChildren<RawImage>();
 
         public Button loadButton => GetComponentsInChildren<Button>()[0];
         public Button deleteButton => GetComponentsInChildren<Button>()[1];
+
+        [Inject]
+        private void Construct(ISaveService saveService)
+        {
+            _saveService = saveService;
+        }
+
+        public void Setup(LocalSave localSave)
+        {
+            preview.texture = localSave.Preview;
+        }
     }
 }
