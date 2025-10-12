7d4830beca248cb7863ae01d0af1e2f231e78516
Pavlo Hornostaiev <goronostaev@gmail.com>
2025-10-12 15:31:01 +0300

    Added widget for texture painting

---
+++ Assets/Scripts/UI/Widgets/Painting/TexturePaintingWidget.cs
@@ -0,0 +1,17 @@
+﻿using Services.Painting;
+using UnityEngine;
+using Zenject;
+
+namespace UI.Widgets.Painting
+{
+	public class TexturePaintingWidget : MonoBehaviour
+	{
+		private IModelPaintingService _modelPaintingService;
+
+		[Inject]
+		private void Construct(IModelPaintingService modelPaintingService)
+		{
+			_modelPaintingService = modelPaintingService;
+		}
+	}
+}
