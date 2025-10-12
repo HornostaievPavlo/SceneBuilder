dbdec8d1f71d90a485a335b5bba812d17407aa68
Pavlo Hornostaiev <goronostaev@gmail.com>
2025-10-12 15:30:29 +0300

    Added painting widget

---
+++ Assets/Scripts/UI/Widgets/Painting/PaintingWidget.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.Widgets.Painting
+{
+	public class PaintingWidget : MonoBehaviour
+	{
+		[SerializeField] private Button colorPaintingButton;
+		[SerializeField] private Button texturePaintingButton;
+		
+		[SerializeField] private ColorPaintingWidget colorPaintingWidget;
+		[SerializeField] private TexturePaintingWidget texturePaintingWidget;
+
+		private void OnEnable()
+		{
+			colorPaintingButton.onClick.AddListener(HandleColorPaintingButtonClicked);
+			texturePaintingButton.onClick.AddListener(HandleTexturePaintingButtonClicked);
+		}
+		
+		private void OnDisable()
+		{
+			colorPaintingButton.onClick.RemoveListener(HandleColorPaintingButtonClicked);
+			texturePaintingButton.onClick.RemoveListener(HandleTexturePaintingButtonClicked);
+		}
+
+		private void HandleColorPaintingButtonClicked()
+		{
+			colorPaintingWidget.gameObject.SetActive(true);
+			texturePaintingWidget.gameObject.SetActive(false);
+		}
+
+		private void HandleTexturePaintingButtonClicked()
+		{
+			texturePaintingWidget.gameObject.SetActive(true);
+			colorPaintingWidget.gameObject.SetActive(false);
+		}
+	}
+}
