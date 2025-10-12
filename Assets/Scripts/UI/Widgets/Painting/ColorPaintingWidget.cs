419d5c79806ea0a8bb6fe075bf0e0994a1ef8dd4
Pavlo Hornostaiev <goronostaev@gmail.com>
2025-10-12 15:55:58 +0300

    Added colors buttons setup in color painting widget

--- Assets/Scripts/UI/Widgets/Painting/ColorPaintingWidget.cs
+++ Assets/Scripts/UI/Widgets/Painting/ColorPaintingWidget.cs
@@ -12,7 +12,7 @@ namespace UI.Widgets.Painting
 		[SerializeField] private Button resetChangesButton;
 
 		private Color[] _colors;
-		private Button[] _colorsButtons;
+		private Button[] _colorButtons;
 		
 		private IModelPaintingService _modelPaintingService;
 
@@ -25,19 +25,54 @@ namespace UI.Widgets.Painting
 		private void Awake()
 		{
 			_colors = Constants.PaintingColors;
-			_colorsButtons = colorsButtonsParent.GetComponentsInChildren<Button>();
+			SetupColorButtons();
 		}
 		
 		private void OnEnable()
 		{
 			colorTintSlider.onValueChanged.AddListener(HandleColorTintSliderValueChanged);
 			resetChangesButton.onClick.AddListener(HandleResetChangesButtonClicked);
+			AddColorButtonsHandlers();
 		}
 
 		private void OnDisable()
 		{
 			colorTintSlider.onValueChanged.RemoveListener(HandleColorTintSliderValueChanged);
 			resetChangesButton.onClick.RemoveListener(HandleResetChangesButtonClicked);
+			RemoveColorButtonsHandlers();
+		}
+
+		private void SetupColorButtons()
+		{
+			_colorButtons = colorsButtonsParent.GetComponentsInChildren<Button>();
+			
+			for (int i = 0; i < _colors.Length; i++)
+			{
+				Image buttonImage = _colorButtons[i].GetComponent<Image>();
+				buttonImage.color = _colors[i];
+			}
+		}
+
+		private void AddColorButtonsHandlers()
+		{
+			for (int i = 0; i < _colors.Length; i++)
+			{
+				int colorIndex = i;
+				_colorButtons[i].onClick.AddListener(() => HandleColorButtonClicked(colorIndex));
+			}
+		}
+
+		private void RemoveColorButtonsHandlers()
+		{
+			for (int i = 0; i < _colorButtons.Length; i++)
+			{
+				_colorButtons[i].onClick.RemoveAllListeners();
+			}
+		}
+
+		private void HandleColorButtonClicked(int colorIndex)
+		{
+			_modelPaintingService.SetColor(_colors[colorIndex]);
 		}
 
 		private void HandleColorTintSliderValueChanged(float value)
