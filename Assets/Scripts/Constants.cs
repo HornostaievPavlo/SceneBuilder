149c3f230fd6d9be88793d20371dc6c2b06a3534
Pavlo Hornostaiev <goronostaev@gmail.com>
2025-10-19 14:37:26 +0300

    Removed unused field

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -5,7 +5,6 @@ public static class Constants
 	public const string DuckModelPath = "https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/Duck/glTF/Duck.gltf";
 
 	public const string AssetFile = "/Asset.gltf";
-	public const string TextureFile = "/Texture.png";
 	public const string PreviewFile = "/Preview.png";
 	
 	public static readonly string ApplicationDataPath = Application.persistentDataPath;
