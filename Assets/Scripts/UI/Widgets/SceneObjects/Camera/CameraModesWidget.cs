703006917b2fb02513e1788f514b7fa170346266
Pavlo Hornostaiev <goronostaev@gmail.com>
2025-10-19 12:26:11 +0300

    Added animations for camera rect size on mode change

--- Assets/Scripts/UI/Widgets/SceneObjects/Camera/CameraModesWidget.cs
+++ Assets/Scripts/UI/Widgets/SceneObjects/Camera/CameraModesWidget.cs
@@ -4,6 +4,7 @@ using Services.SceneObjectSelection;
 using UnityEngine;
 using UnityEngine.UI;
 using Zenject;
+using DG.Tweening;
 
 namespace UI.Widgets.SceneObjects.Camera
 {
@@ -35,6 +36,11 @@ namespace UI.Widgets.SceneObjects.Camera
 		private CameraModeTypeId _currentModeTypeId;
 
 		private ISceneObjectSelectionService _sceneObjectSelectionService;
+
+		private const float RectTweenDuration = 0.1f;
+		
+		private Tweener _mainCameraRectTween;
+		private Tweener _sceneObjectCameraRectTween;
 
 		[Inject]
 		private void Construct(ISceneObjectSelectionService sceneObjectSelectionService)
@@ -136,7 +142,7 @@ namespace UI.Widgets.SceneObjects.Camera
 
 		private void SetCameraViewMode()
 		{
-			_sceneObjectCamera.rect = new Rect(0, 0, 1, 1);
+			AnimateCameraRect(_sceneObjectCamera, new Rect(0, 0, 1, 1));
 			_sceneObjectCamera.depth = Constants.MiddleCameraDepth;
 
 			_mainCamera.depth = Constants.ZeroCameraDepth;
@@ -148,8 +154,8 @@ namespace UI.Widgets.SceneObjects.Camera
 
 		private void SetSplitScreenMode()
 		{
-			_mainCamera.rect = new Rect(0, 0, 0.5f, 1);
-			_sceneObjectCamera.rect = new Rect(0.5f, 0, 0.5f, 1);
+			AnimateCameraRect(_mainCamera, new Rect(0, 0, 0.5f, 1));
+			AnimateCameraRect(_sceneObjectCamera, new Rect(0.5f, 0, 0.5f, 1));
 
 			_sceneObjectCamera.depth = Constants.LowestCameraDepth;
 
@@ -160,7 +166,7 @@ namespace UI.Widgets.SceneObjects.Camera
 
 		private void SetMainViewMode()
 		{
-			_mainCamera.rect = new Rect(0, 0, 1, 1);
+			AnimateCameraRect(_mainCamera, new Rect(0, 0, 1, 1));
 
 			_sceneObjectCamera.depth = Constants.ZeroCameraDepth;
 			_mainCamera.depth = Constants.HighestCameraDepth;
@@ -168,6 +174,24 @@ namespace UI.Widgets.SceneObjects.Camera
 			mainViewImage.sprite = mainViewImageSelected;
 			splitScreenViewImage.sprite = splitScreenImageDeselected;
 			cameraViewImage.sprite = cameraViewImageDeselected;
+		}
+
+		private void AnimateCameraRect(UnityEngine.Camera camera, Rect targetRect)
+		{
+			if (camera == _mainCamera)
+			{
+				_mainCameraRectTween?.Kill();
+				_mainCameraRectTween = camera.DORect(targetRect, RectTweenDuration)
+					.SetEase(Ease.Linear)
+					.SetUpdate(UpdateType.Late);
+			}
+			else if (camera == _sceneObjectCamera)
+			{
+				_sceneObjectCameraRectTween?.Kill();
+				_sceneObjectCameraRectTween = camera.DORect(targetRect, RectTweenDuration)
+					.SetEase(Ease.Linear)
+					.SetUpdate(UpdateType.Late);
+			}
 		}
 	}
 }
